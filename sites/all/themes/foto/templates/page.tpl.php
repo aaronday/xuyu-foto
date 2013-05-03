8e5f1ec77939197e06b8e6c375b8e65323a80ee0
aaron.dai <aaron.dai@perficientgdc.com.cn>
2013-05-03 16:26:38 +0800

    May 3th commit

--- sites/all/themes/foto/templates/page.tpl.php
+++ sites/all/themes/foto/templates/page.tpl.php
@@ -1,11 +1,19 @@
+<?php
+$root_path = 'http://' . $_SERVER['HTTP_HOST'] . base_path();
+$node = menu_get_object();
+$nid = -1;
+if ($node) {
+    $nid = $node->nid;
+}
+?>
 <div id="page-wrapper" class="fullheight allfont">
     <div id="page" class="fullheight">
         <div id="header-wrapper">
             <div id="top-bar">
                 <div id="social-icons">
-                    <div class="social-icon"><a href="" ><img src="<?php echo drupal_get_path('theme', 'foto') ?>/images/facebook.png" width="20px" height="20px"></a></div>
-                    <div class="social-icon"><a href="" ><img src="<?php echo drupal_get_path('theme', 'foto') ?>/images/twitter.png" width="20px" height="20px"></a></div>
-                    <div class="social-icon"><a href="" ><img src="<?php echo drupal_get_path('theme', 'foto') ?>/images/flickr.png" width="20px" height="20px"></a></div>
+                    <div class="social-icon"><a href="" ><img src="<?php echo $root_path . drupal_get_path('theme', 'foto') ?>/images/facebook.png" width="20px" height="20px"></a></div>
+                    <div class="social-icon"><a href="" ><img src="<?php echo $root_path . drupal_get_path('theme', 'foto') ?>/images/twitter.png" width="20px" height="20px"></a></div>
+                    <div class="social-icon"><a href="" ><img src="<?php echo $root_path . drupal_get_path('theme', 'foto') ?>/images/flickr.png" width="20px" height="20px"></a></div>
                 </div>
                 <div id="sign-in">
                     <div class="sign-back">
@@ -19,7 +27,7 @@
                     <div class="name-en">aaron-photograph</div>
                 </div>
                 <div id="site-date">
-                    <div class="date-img"><img src="<?php echo drupal_get_path('theme', 'foto') ?>/images/planner.png"></div>
+                    <div class="date-img"><img src="<?php echo $root_path . drupal_get_path('theme', 'foto') ?>/images/planner.png"></div>
                     <div class="date-number">四月 廿四</div>
                 </div>
             </div>
@@ -45,68 +53,11 @@
         </div>
         <div class="clear"></div>
         <div id="main-wrapper">
-            <div id="main-content">
-                <div id="intro-and-register">
-                    <div id="intro">
-                        <div id="intro-title">
-                            须臾是一种态度
-                        </div>
-                        <div id="intro-body">
-                            刹那者为一念，二十念为一瞬，二十瞬为一弹指，二十弹指为一罗预，二十罗预为一须臾，一日一夜为三十须臾。
-                            <br />
-                            <br />
-                            《仪礼·燕礼》：“寡君有不腆之酒，以请吾子之与寡君须臾焉，使某也以请。”《文选·离骚》：“折若木以拂日兮，聊须臾以相羊。” 李善注引 王逸曰：“须臾、相羊，皆游也。”一本作“ 逍遥 ”。
-                        </div>
-                    </div>
-                    <div id="register">
-                        <div class="register-label">注册</div>
-                        <div style="font-size: 12px; margin: 0px 0px 0px 12px;"></div>
-                    </div>
+            <?php if ($page['content']['system_main']['nodes'][$nid]['#bundle'] == 'blog'): ?>
+                <div id="blog-main" class="transparent-content">
+                    <?php print render($page['content']); ?>
                 </div>
-                <div id="contact-info">
-                    <div id="location-and-phone" class="contact-matrix">
-                        <div id="location" class="contact-matrix-big" style="background-color: black; ">
-                            <div class="matrix-big-font">地址</div>
-                            <div class="matrix-small-font">啦啦啦，这里是地址</div>
-                        </div>
-                        <div id="phone" class="contact-matrix-small" style="background-color: #e4365b;">
-                            <div><img src=""/></div> 
-                            <div style="margin: 5px 0px 0px 10px; font-size: 12px;">0123456789</div>
-                        </div>
-                    </div>
-                    <div id="schedule" class="contact-matrix" style="margin-left: 10px;">
-                        <div id="" class="contact-matrix-big" style="background-color: #2978ee;">
-                            <div class="matrix-big-font">日程</div>
-                            <div class="matrix-small-font">啦啦啦，这里是日程</div>
-                        </div>
-                        <div id="" class="contact-matrix-small transparent-content">
-
-                        </div>
-                    </div>
-                    <div id="trip" class="transparent-content">
-                    </div>
-                </div>
-                <div id="img-node">
-                    <div class="img-matrix">
-                        <div><img src="<?php echo drupal_get_path('theme', 'foto') ?>/images/background1.jpg" width="220px" height="150px;"/></div>
-                        <div class="img-matrix-label">
-                            <div style="padding: 5px;">按时发放热水</div>
-                        </div>
-                    </div>
-                    <div class="img-matrix" style="margin-left: 10px;">
-                        <div><img src="<?php echo drupal_get_path('theme', 'foto') ?>/images/background2.jpg" width="220px" height="150px;"/></div>
-                        <div class="img-matrix-label">
-                            <div style="padding: 5px;">按时发放热水</div>
-                        </div>
-                    </div>
-                    <div class="img-matrix" style="margin-left: 10px;">
-                        <div><img src="<?php echo drupal_get_path('theme', 'foto') ?>/images/background1.jpg" width="220px" height="150px;"/></div>
-                        <div class="img-matrix-label">
-                            <div style="padding: 5px;">按时发放热水</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php endif; ?>
         </div>
         <div class="clear"></div>
         <div id="footer-wrapper">
